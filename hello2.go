9083b42255e7f600c44bbb14b03ee55b0c29acfa
khalilsediq <khalilullahsadique1334@gmail.com>
2026-05-18 12:37:01 +0500

    add initial Go module and Makefile, update hello2.go structure

--- hello2.go
+++ hello2.go
@@ -1,5 +1,13 @@
-package main 
-import "fmt" 
-func start() {
-fmt.Println("Hello world! This is khalilullah")
+// package main
+// import "fmt"
+// func start() {
+// fmt.Println("Hello world! This is khalilullah")
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
 }
