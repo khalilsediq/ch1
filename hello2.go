b5aa560b319354d25125171ba6a26f8c94c4536c
khalilsediq <khalilullahsadique1334@gmail.com>
2026-05-18 17:32:26 +0500

    refactor hello2.go to comment out unused variables and text output

--- hello2.go
+++ hello2.go
@@ -23,8 +23,16 @@ func main() {
 	// var num2 int8 = 60
 	// fmt.Println(num1  , num2)
 
-	var text = `I am khalilullah,
-	 I am a software Engineer and the best programmer in the world.
-	  I am literally unstopable`
-	fmt.Println(text)
+	// var text = `I am khalilullah,
+	//  I am a software Engineer and the best programmer in the world.
+	//   I am literally unstopable`
+	// fmt.Println(text)
+
+	// var num int8 = 20
+	// var num2 = 10
+	var num3 = 4.5
+	var num4 = 5.5
+	fmt.Println(num3 - num4)
+
+	// fmt.Println(num + int8(num2) + int8(num3))
 }
