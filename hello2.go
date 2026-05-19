e7d97f15ce59aaaab1a904bd2e1961309f1ec53c
khalilsediq <khalilullahsadique1334@gmail.com>
2026-05-19 12:34:04 +0500

    Done

--- hello2.go
+++ hello2.go
@@ -8,65 +8,87 @@ package main
 
 import "fmt"
 
+//func main() {
+// fmt.Println("Hello\nWorld")
+// fmt.Println("Greetings and\nSalutations")
+
+// var name = "khalilullah"
+// var age = 20
+// var num1 = 50
+// var num2 = 30
+// fmt.Println(num1 + num2)
+// fmt.Println(name, age)
+
+// var num1 float32 = 40.4
+// var num2 int8 = 60
+// fmt.Println(num1  , num2)
+
+// var text = `I am khalilullah,
+//  I am a software Engineer and the best programmer in the world.
+//   I am literally unstopable`
+// fmt.Println(text)
+
+// var num int8 = 20
+// var num2 = 10
+// var num3 = 4.5
+// var num4 = 5.5
+// fmt.Println(num3 - num4)
+
+// fmt.Println(num + int8(num2) + int8(num3))
+
+// var x = 18
+// if 10 >= 18 {
+// 	fmt.Println("you are eligible, for the club.")
+
+// } else {
+// 	fmt.Println("Sorry, you are not eligible for the Club")
+// }
+
+// var x = int  <= this is declared as a 0 value.
+
+// var x, y int = 20, 50 <= we can declare multiple variables at once and they can be of the same type.
+
+// var x, y int //All zero values of the same types
+
+// var x, y = 20, "Khalilullah"  // <= All values of different types
+
+// fmt.Println(x, y)
+
+// var (
+// 	x    int
+// 	y        = 20
+// 	z    int = 40
+// 	d, e     = 50, "Khalilullah"
+// 	f, g string
+// )
+// fmt.Println(x, y, z, d, e, f, g)
+
+// x := 40
+// // x, y := 30, "khalilThePro"
+// x, y := 50, `ХалилПрограммист` // <=  We can only do this on := this. ANd we can not achieve this by the = sign.
+
+// fmt.Println(x, y)
+//}
+
+const x = 49
+
+const (
+	idKey   = "id"
+	nameKey = "name"
+)
+
+const z = 3 * 3
+
 func main() {
-	// fmt.Println("Hello\nWorld")
-	// fmt.Println("Greetings and\nSalutations")
-
-	// var name = "khalilullah"
-	// var age = 20
-	// var num1 = 50
-	// var num2 = 30
-	// fmt.Println(num1 + num2)
-	// fmt.Println(name, age)
-
-	// var num1 float32 = 40.4
-	// var num2 int8 = 60
-	// fmt.Println(num1  , num2)
-
-	// var text = `I am khalilullah,
-	//  I am a software Engineer and the best programmer in the world.
-	//   I am literally unstopable`
-	// fmt.Println(text)
-
-	// var num int8 = 20
-	// var num2 = 10
-	// var num3 = 4.5
-	// var num4 = 5.5
-	// fmt.Println(num3 - num4)
-
-	// fmt.Println(num + int8(num2) + int8(num3))
-
-	// var x = 18
-	// if 10 >= 18 {
-	// 	fmt.Println("you are eligible, for the club.")
-
-	// } else {
-	// 	fmt.Println("Sorry, you are not eligible for the Club")
-	// }
-
-	// var x = int  <= this is declared as a 0 value.
-
-	// var x, y int = 20, 50 <= we can declare multiple variables at once and they can be of the same type.
-
-	// var x, y int //All zero values of the same types
-
-	// var x, y = 20, "Khalilullah"  // <= All values of different types
-
-	// fmt.Println(x, y)
-
-	// var (
-	// 	x    int
-	// 	y        = 20
-	// 	z    int = 40
-	// 	d, e     = 50, "Khalilullah"
-	// 	f, g string
-	// )
-	// fmt.Println(x, y, z, d, e, f, g)
-
-	x := 40
-	// x, y := 30, "khalilThePro"
-	x, y := 50, `ХалилПрограммист` // <=  We can only do this on := this. ANd we can not achieve this by the = sign.
-
-	fmt.Println(x, y)
+	var y = "hello"
+	fmt.Println(x)
+	fmt.Println(y)
+	// fmt.Println(z)
+	// var b = x + 1
+	// var f = "bye"
+	// fmt.Println(b)
+	// fmt.Println(f)
+	y = "bye"
+	fmt.Println(y)
 
 }
